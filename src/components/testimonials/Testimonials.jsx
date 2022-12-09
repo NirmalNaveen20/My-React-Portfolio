7b8223d3cebfad89e794e9370b41366702cb2a93
Nirmal Naveen <53178630+NirmalNaveen20@users.noreply.github.com>
2022-12-10 00:10:37 +0530

    hard code array in js

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import './testimonials.css'
 import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Show',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, vel. Nisi officia laborum dolores non dolorem molestiae! Ipsa, sed ad'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Engin Nora',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, vel. Nisi officia laborum dolores non dolorem molestiae! Ipsa, sed ad'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Millie Bobby',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, vel. Nisi officia laborum dolores non dolorem molestiae! Ipsa, sed ad'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Jatena Morgan',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, vel. Nisi officia laborum dolores non dolorem molestiae! Ipsa, sed ad'
+  },
+]
 
 const Testimonials = () => {
   return (
@@ -9,11 +35,19 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonials">
-          <div className="client__avatar">
-            <img src="" alt="" />
-          </div>
-        </article>
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <article key={index} className="testimonials">
+              <div className="client__avatar">
+                <img src={avatar} />
+              </div>
+              <h5 className='client__name'> {name} </h5>
+              <small className='client__review'> {review} </small>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )

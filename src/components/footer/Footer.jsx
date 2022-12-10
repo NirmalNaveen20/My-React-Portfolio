90098ac33fce9c2224c65eb6bdc5704c8b9bb222
Nirmal Naveen <53178630+NirmalNaveen20@users.noreply.github.com>
2022-12-11 00:23:24 +0530

    completed footer component and some css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {GrFacebook} from 'react-icons/gr'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>My Logo</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><GrFacebook /></a>
+        <a href="https://github.com"><BsGithub /></a>
+        <a href="https://linkdin.com"><BsLinkedin /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&Copy; Nirmal Naveen. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
